e9be03144cfd8834500ceb6b515c4bacc698ff86
Chris Morrell <inxilpro@users.noreply.github.com>
2020-01-03 12:26:52 -0500

    Only lint test files in test/ directory

--- src/Linters/PrefixTestsWithTest.php
+++ src/Linters/PrefixTestsWithTest.php
@@ -2,26 +2,33 @@
 
 namespace Glhd\LaraLint\Linters;
 
+use Glhd\LaraLint\Contracts\FilenameAwareLinter;
 use Glhd\LaraLint\Linters\Helpers\FlagsNodesByRuleset;
 use Glhd\LaraLint\Linters\Strategies\ClassMethodLinter;
 use Microsoft\PhpParser\Node\MethodDeclaration;
 use Microsoft\PhpParser\Node\Statement\ClassDeclaration;
 use Microsoft\PhpParser\TokenKind;
 
-class PrefixTestsWithTest extends ClassMethodLinter
+class PrefixTestsWithTest extends ClassMethodLinter implements FilenameAwareLinter
 {
 	use FlagsNodesByRuleset;
 	
-	protected const MESSAGE = 'Test methods must start with test_';
+	protected $active = true;
+	
+	public function setFilename(string $filename) : void
+	{
+		$this->active = false !== strpos($filename, base_path('tests'));
+	}
 	
 	protected function shouldWalkClass(ClassDeclaration $node) : bool
 	{
-		return preg_match('/Test$/', $node->getNamespacedName()->getFullyQualifiedNameText());
+		return $this->active
+			&& preg_match('/Test$/', $node->getNamespacedName()->getFullyQualifiedNameText());
 	}
 	
 	protected function enterMethod(MethodDeclaration $node) : void
 	{
-		$this->flagNodeIfAllRulesMatch($node, static::MESSAGE, [
+		$this->flagNodeIfAllRulesMatch($node, 'Test methods must start with test_', [
 			false === $node->isStatic(),
 			false === $node->hasModifier(TokenKind::ProtectedKeyword),
 			false === $node->hasModifier(TokenKind::PrivateKeyword),
