d436c43cda576b1a7bb797f97da31d7e53a59ee8
Chris Morrell <inxilpro@users.noreply.github.com>
2020-01-03 13:57:23 -0500

    Added match collection strategy

--- src/Linters/OrderUseStatementsAlphabetically.php
+++ src/Linters/OrderUseStatementsAlphabetically.php
@@ -3,48 +3,35 @@
 namespace Glhd\LaraLint\Linters;
 
 use Glhd\LaraLint\Contracts\Matcher;
-use Glhd\LaraLint\Linters\Strategies\MatchingLinter;
+use Glhd\LaraLint\Linters\Strategies\MatchCollectingLinter;
 use Glhd\LaraLint\Result;
 use Glhd\LaraLint\ResultCollection;
 use Illuminate\Support\Collection;
-use Microsoft\PhpParser\Node;
 use Microsoft\PhpParser\Node\Statement\NamespaceUseDeclaration;
 
-class OrderUseStatementsAlphabetically extends MatchingLinter
+class OrderUseStatementsAlphabetically extends MatchCollectingLinter
 {
-	/**
-	 * @var NamespaceUseDeclaration[]
-	 */
-	protected $collected_nodes = [];
-	
-	public function lint() : ResultCollection
+	public function lintCollectedNodes(Collection $all_nodes) : ResultCollection
 	{
-		$all_nodes = new Collection($this->collected_nodes);
-		$all_statements = $all_nodes->map(function(Node $node) {
-			return strtolower($node->getText());
-		});
-		$ordered_statements = $all_statements->sort();
+		$results = new ResultCollection();
 		
-		if ($all_statements->implode(' ') !== $ordered_statements->implode(' ')) {
-			return new ResultCollection(
-				$all_nodes->map(function(NamespaceUseDeclaration $node) {
-					return new Result($node, 'Use statements should be ordered alphabetically.');
-				})
-			);
+		$last_statement = '';
+		foreach ($all_nodes as $i => $node) {
+			$statement = strtolower($node->getText());
+			
+			if ($i > 0 && $last_statement > $statement) {
+				$results->push(new Result($node, 'Use statements should be ordered alphabetically.'));
+				break;
+			}
+			
+			$last_statement = $statement;
 		}
 		
-		return new ResultCollection([]);
+		return $results;
 	}
 	
 	protected function matcher() : Matcher
 	{
 		return $this->orderedMatcher()->withChild(NamespaceUseDeclaration::class);
-	}
-	
-	protected function onMatch(Collection $nodes) : ?Result
-	{
-		$this->collected_nodes[] = $nodes->first();
-		
-		return null;
 	}
 }
---
+++ src/Linters/Strategies/MatchCollectingLinter.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Glhd\LaraLint\Linters\Strategies;
+
+use Glhd\LaraLint\Result;
+use Glhd\LaraLint\ResultCollection;
+use Illuminate\Support\Collection;
+use Microsoft\PhpParser\Node;
+
+abstract class MatchCollectingLinter extends MatchingLinter
+{
+	protected $collected_nodes;
+	
+	public function __construct()
+	{
+		parent::__construct();
+		
+		$this->collected_nodes = new Collection();
+	}
+	
+	public function lint() : ResultCollection
+	{
+		return $this->lintCollectedNodes($this->collected_nodes);
+	}
+	
+	protected function onMatch(Collection $nodes) : ?Result
+	{
+		$this->collected_nodes->push($this->reduceMatchedNodes($nodes));
+		
+		return null;
+	}
+	
+	protected function reduceMatchedNodes(Collection $nodes) : Node
+	{
+		return $nodes->first();
+	}
+	
+	abstract protected function lintCollectedNodes(Collection $nodes) : ResultCollection; 
+}
